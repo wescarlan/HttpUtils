0558b62270e7f7439feebd70f598f1f5d38c670f
Wesley Carlan <wescarlan13@gmail.com>
2020-08-16 15:39:59 -0700

    Create Swift package

---
+++ Sources/HttpUtils/HTTP.swift
@@ -0,0 +1,14 @@
+//
+//  HTTP.swift
+//  HttpUtils
+//
+//  Created by Wesley on 8/16/20.
+//
+
+public struct HTTP {
+    public struct Header {
+        public struct Value {}
+    }
+    
+    public struct StatusCode {}
+}
---
+++ Tests/HttpUtilsTests/HttpUtilsTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import HttpUtils
+
+final class HttpUtilsTests: XCTestCase {
+    func testExample() {
+        
+    }
+
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
